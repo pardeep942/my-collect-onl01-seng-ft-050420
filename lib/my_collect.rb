c74ddad4ddc5168872e5b6999cbba145aef6c200
PARDEEP KAUR <pkaur84@aol.com>
2020-05-14 19:43:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,4 +2,15 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
 end
+def my_collect(array) # put argument(s) here
+  # code here
+  i = 0
+  result = []
 
+  while i < array.size
+    block_given? # optional
+    result << yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
+    i += 1
+  end
+result
+end
